14c6e7d106aeb2f98bc4a8778876866ca0f9bc10
Bertram Petersen <tilbertrampetersen@gmail.com>
2022-10-20 04:11:02 +0200

    Jeg er i tvivl om, hvordan man laver metoden add og substract,

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Wallet conis =new Wallet(0);
+        conis.print_Wallet_data();
     }
+
+
+
 }
--- src/Wallet.java
+++ src/Wallet.java
@@ -1,2 +1,33 @@
 public class Wallet {
+    public int coins=0;
+
+    public void Wallet(int coins){
+        this.coins=coins;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+
+    public void addCoions() {
+
+if (answerCorrect){
+    addCoions();
 }
+    }
+
+
+    public void substractCoins(){
+if ( builtEnergySource){
+    substractCoins();
+}
+    }
+
+}
+
+
