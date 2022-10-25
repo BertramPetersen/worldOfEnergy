25bd1db46dd71c5d5fcf68444b2cfb89189376fc
Marcus <Marcussand2@gmail.com>
2022-10-25 09:12:21 +0200

    Quiz

--- src/RandomEvent.java
+++ src/RandomEvent.java
@@ -1,2 +1,28 @@
 public class RandomEvent {
+    String description;
+    int impact;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImpact() {
+        return impact;
+    }
+
+    public void setImpact(int impact) {
+        this.impact = impact;
+    }
+
+    public FutureForecast setFutureForecast (FutureForecast forecast) {
+        this.forecast = forecast;
+    }
+
+    public void ImpactPlayer(){
+
+    }
 }
