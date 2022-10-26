6270c5e29948eabd7fa258de713dd5fb33b21a62
Bertram Petersen <tilbertrampetersen@gmail.com>
2022-10-26 12:13:43 +0200

    Fucking mange ændringer 25-10-2022

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        Wallet conis =new Wallet();
-
+        Game game = new Game();
+        Room s = game.roomMap.get("Scandinavia");
+        s.viewPotentials();
+        s.constructWind();
+        System.out.println("You now have this many coins: " + Wallet.getCoins());
+        System.out.println();
+        game.updateTurn();
+//        EnergyBalance.show();
+//        System.out.println();
+//        for (int i = 0; i < 7; i++) {
+//            game.quiz.takeQuiz();
+//        }
+//        game.updateTurn();
     }
-
-
-
 }
--- src/WindMill.java
+++ src/WindMill.java
@@ -1,2 +1,7 @@
 public class WindMill extends EnergySource {
+    public WindMill() {
+        super.price = 100;
+        super.output = 0.4;
+        super.type = "Wind";
+    }
 }
